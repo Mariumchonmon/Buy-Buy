6ca96f9294737f7d3eea8316056fdba5f6b453a1
Mariumchonmon <Mariumakterch@gmail.com>
2021-08-23 19:24:10 +0600

    step-1

---
+++ lib/screens/item_overview_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class ItemOverviewScreen extends StatelessWidget {
+  const ItemOverviewScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
